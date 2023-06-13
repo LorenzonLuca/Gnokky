51b01e4a6dd4cb35bf40c09d67fba968848e7228
LorenzonLuca <luca.lorenzon@samtrevano.ch>
2023-06-13 15:34:15 +0100

    button

--- Application/gnokky/components/GN/GNButton.js
+++ Application/gnokky/components/GN/GNButton.js
@@ -1,7 +1,8 @@
 import React, { useState, useRef } from 'react';
 import { TouchableWithoutFeedback, Text, View, StyleSheet, Animated } from 'react-native';
+import { COLORS } from '../Models/Globals';
 
-export default function GNButton({ title, backgroundColor = '#F8D154', color = '#25292e',
+export default function GNButton({ title, backgroundColor = COLORS.elements, color = COLORS.firtText,
   width = '75%', height = 50, onPress, marginBottom = 24, style, }) {
 
   const styles = StyleSheet.create({
@@ -13,7 +14,6 @@ export default function GNButton({ title, backgroundColor = '#F8D154', color = '
     button: {
       flex: 1,
       borderRadius: 15,
-      color: '#333',
       width: width,
       height: height,
       alignItems: 'center',
