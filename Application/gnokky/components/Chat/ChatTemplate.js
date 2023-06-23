d1e5adaa85cf1dcdd2a7c967684fce6e258e85e3
LorenzonLuca <luca.lorenzon@samtrevano.ch>
2023-06-23 15:01:30 +0100

    pana lavora

--- Application/gnokky/components/Chat/ChatTemplate.js
+++ Application/gnokky/components/Chat/ChatTemplate.js
@@ -1,10 +1,24 @@
-import { SafeAreaView, ScrollView, View, StyleSheet, Text } from "react-native";
+import { SafeAreaView, ScrollView, View, StyleSheet, Text, TextInput } from "react-native";
+import GNButton from '../GN/GNButton';
 import { COLORS, appUser } from '../Models/Globals';
+import { useState } from "react";
 
 
 export default function ChatTemplate({ navigation, route }) {
     const { user } = route.params;
     console.log("OOOOOOOOOOOOOOOOOOOOOOOOOOOO", user);
+    const [message, setMessage] = useState("");
+
+    const isTextInputValid = message.trim().length === 0;
+
+
+    const handleWriteMessage = (text) => {
+        setMessage(text);
+    }
+
+    const handleSendMessage = () => {
+
+    }
 
     const styles = StyleSheet.create({
         container: {
@@ -19,18 +33,50 @@ export default function ChatTemplate({ navigation, route }) {
         header: {
             paddingVertical: 25,
         },
+        footer: {
+            width: '100%',
+            flexDirection: 'row',
+            alignItems: 'center',
+            borderTopWidth: 1,
+            borderTopColor: '#CCCCCC',
+            paddingVertical: 8,
+        },
         body: {
             flex: 1,
             padding: 20,
         },
+        messageInput: {
+            flex: 1,
+            marginRight: 8,
+            borderWidth: 1,
+            borderColor: '#CCCCCC',
+            borderRadius: 15,
+            padding: 8,
+            height: '100%',
+        }
     });
-
 
     return (
         <SafeAreaView style={styles.container}>
             <ScrollView contentContainerStyle={styles.contentContainer}>
                 <View style={styles.body}>
                     <Text>Coddio</Text>
+                </View>
+                <View style={styles.footer}>
+                    <TextInput
+                        style={styles.messageInput}
+                        placeholder="Write message"
+                        value={message}
+                        onChangeText={handleWriteMessage}
+                        multiline={true}
+                    />
+                    <GNButton
+                        title={'SEND'}
+                        backgroundColor={isTextInputValid ? COLORS.thirdText : COLORS.elements}
+                        isDisabled={isTextInputValid}
+                        width={'15%'}
+                        onPress={handleSendMessage} />
+
                 </View>
             </ScrollView>
         </SafeAreaView>
