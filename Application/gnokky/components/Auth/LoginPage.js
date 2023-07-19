dce89a24eccad6b144d80fc2e53f3c48e9b12db9
Stefano Panarelli <stefano.panarelli@samtrevano.ch>
2023-07-19 11:07:29 +0200

    added keyboard avoiding view

--- Application/gnokky/components/Auth/LoginPage.js
+++ Application/gnokky/components/Auth/LoginPage.js
@@ -1,110 +1,128 @@
+import React, { useState } from 'react';
+import {
+  View,
+  Text,
+  SafeAreaView,
+  Image,
+  KeyboardAvoidingView,
+  ScrollView,
+  StyleSheet,
+} from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import { View, Text, SafeAreaView, KeyboardAvoidingView, Image } from 'react-native';
-import { useState } from 'react';
 import GNButton from '../GN/GNButton';
 import GNTextInput from '../GN/GNTextInput';
 import GNTextInputPassword from '../GN/GNTextInputPassword';
 import { handleLogin } from './AuthUtils';
-import { StyleSheet } from 'react-native';
 import { COLORS, ROUTES, IMAGES } from '../Models/Globals';
 
-
-
 export default function LoginPage({ navigation }) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
 
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [error, setError] = useState('');
+  const logo = IMAGES.LOGO;
 
-    const logo = IMAGES.LOGO;
+  const styles = StyleSheet.create({
+    safeAreaContainer: {
+      flex: 1,
+    },
+    errorText: {
+      color: '#F00',
+      textAlign: 'center',
+      marginVertical: 35,
+    },
+    text: {
+      textAlign: 'center',
+      color: COLORS.secondText,
+    },
+    forgotPasswordLink: {
+      color: COLORS.elements,
+      fontWeight: 'bold',
+      textAlign: 'right',
+      marginBottom: 40,
+      alignSelf: 'flex-end',
+    },
+    link: {
+      color: COLORS.elements,
+      fontWeight: 'bold',
+    },
+    title: {
+      color: COLORS.elements,
+      fontSize: 65,
+      marginBottom: 120,
+      textAlign: 'center',
+      fontFamily: 'mnst-bold',
+    },
+    keyboard: {
+      flex: 1,
+    },
+    scrollViewContainer: {
+      flexGrow: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    image: {
+      width: 80,
+      height: 80,
+      alignSelf: 'center',
+    },
+  });
 
-    const styles = StyleSheet.create({
-        safeAreaContainer: {
-            flex: 1,
-        },
-        container: {
-            flex: 1,
-            backgroundColor: COLORS.background,
-            alignItems: 'center',
-            justifyContent: 'center',
-        },
-        errorText: {
-            color: '#F00',
-            textAlign: 'center',
-            marginVertical: 35,
-        },
-        text: {
-            textAlign: 'center',
-            color: COLORS.secondText,
-
-        },
-        forgotPasswordLink: {
-            color: COLORS.elements,
-            fontWeight: 'bold',
-            textAlign: 'right',
-            marginBottom: 40,
-        },
-        link: {
-            color: COLORS.elements,
-            fontWeight: 'bold',
-        },
-        title: {
-            color: COLORS.elements,
-            fontSize: 65,
-            marginBottom: 120,
-            textAlign: 'center',
-            fontFamily: 'mnst-bold'
-        },
-        keyboard: {
-            top: 125,
-            flex: 1,
-        },
-        image: {
-            // flex: 1,
-            width: 80,
-            height: 80,
-            alignSelf: 'center',
-            // resizeMode: 'cover',
-        }
-    });
-
-    return (
-        <SafeAreaView style={styles.safeAreaContainer}>
-            <View style={styles.container}>
-                <KeyboardAvoidingView style={styles.keyboard}>
-                    <Image source={logo} style={styles.image} />
-                    <Text style={styles.title}>SIGN IN</Text>
-                    <GNTextInput
-                        placeholder='Email'
-                        iconName="mail-outline"
-                        iconNameFocused="mail"
-                        onChangeText={setEmail}
-                        animation="true" />
-                    <GNTextInputPassword
-                        placeholder='Password'
-                        iconName="lock-closed-outline"
-                        iconNameFocused="lock-closed"
-                        onChangeText={setPassword}
-                        animation="true"
-                        marginBottom={15} />
-                    <Text
-                        style={styles.forgotPasswordLink}
-                        onPress={() => { }}>
-                        Forgot password?
-                    </Text>
-                    <GNButton
-                        title={"SIGN IN"}
-                        onPress={() => { handleLogin(email, password, navigation, setError) }}
-                    />
-                    {/* <Text style={styles.errorText}>{error}</Text> */}
-                    <Text style={styles.text}>
-                        Don't have already an account?
-                        <Text style={styles.link} onPress={() => { navigation.navigate(ROUTES.REGISTER) }}> Sign up now!</Text>
-                    </Text>
-                    {error}
-                    <StatusBar style="dark" />
-                </KeyboardAvoidingView>
+  return (
+    <SafeAreaView style={styles.safeAreaContainer}>
+        <KeyboardAvoidingView
+          behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+          style={styles.keyboard}
+        >
+          <ScrollView
+            contentContainerStyle={styles.scrollViewContainer}
+            keyboardShouldPersistTaps="handled"
+          >
+            <Image source={logo} style={styles.image} />
+            <Text style={styles.title}>SIGN IN</Text>
+            <GNTextInput
+              placeholder="Email"
+              iconName="mail-outline"
+              iconNameFocused="mail"
+              onChangeText={setEmail}
+              animation="true"
+            />
+            <GNTextInputPassword
+              placeholder="Password"
+              iconName="lock-closed-outline"
+              iconNameFocused="lock-closed"
+              onChangeText={setPassword}
+              animation="true"
+              marginBottom={15}
+            />
+            <View style={{width: '75%'}}>
+                <Text style={styles.forgotPasswordLink} onPress={() => {}}>
+                Forgot password?
+                </Text>
             </View>
-        </SafeAreaView>
-    )
-};
+            <GNButton
+              title={'SIGN IN'}
+              onPress={() => {
+                handleLogin(email, password, navigation, setError);
+              }}
+            />
+            <Text></Text>
+            <Text style={styles.text}>
+              Don't have already an account?
+              <Text
+                style={styles.link}
+                onPress={() => {
+                  navigation.navigate(ROUTES.REGISTER);
+                }}
+              >
+                {' '}
+                Sign up now!
+              </Text>
+            </Text>
+            {error}
+            <StatusBar style="dark" />
+          </ScrollView>
+        </KeyboardAvoidingView>
+    </SafeAreaView>
+  );
+}
--- Application/gnokky/components/Auth/RegisterPage.js
+++ Application/gnokky/components/Auth/RegisterPage.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {
+  View,
+  Text,
+  SafeAreaView,
+  Image,
+  KeyboardAvoidingView,
+  ScrollView,
+  StyleSheet,
+} from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import { View, Text, SafeAreaView, StyleSheet, Image } from 'react-native';
-import { useState } from 'react';
 import GNButton from '../GN/GNButton';
 import GNTextInput from '../GN/GNTextInput';
 import GNTextInputPassword from '../GN/GNTextInputPassword';
@@ -9,106 +16,123 @@ import { handleRegister } from './AuthUtils';
 import { COLORS, ROUTES, IMAGES } from '../Models/Globals';
 
 export default function RegisterPage({ navigation }) {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [password2, setPassword2] = useState('');
+  const [error, setError] = useState('');
 
+  const logo = IMAGES.LOGO;
 
-    const [username, setUsername] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [password2, setPassword2] = useState('');
-    const [error, setError] = useState('');
+  const styles = StyleSheet.create({
+    safeAreaContainer: {
+      flex: 1,
+    },
+    container: {
+      flex: 1,
+      backgroundColor: COLORS.background,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    errorText: {
+      color: '#F00',
+      textAlign: 'center',
+      marginVertical: 35,
+    },
+    text: {
+      textAlign: 'center',
+      color: COLORS.secondText,
+    },
+    link: {
+      color: COLORS.elements,
+      fontWeight: 'bold',
+    },
+    title: {
+      color: COLORS.elements,
+      fontSize: 65,
+      marginBottom: 80,
+      textAlign: 'center',
+      fontFamily: 'mnst-bold',
+    },
+    keyboard: {
+      flex: 1,
+    },
+    scrollViewContainer: {
+      flexGrow: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    image: {
+      width: 80,
+      height: 80,
+      alignSelf: 'center',
+    },
+  });
 
-    const logo = IMAGES.LOGO;
-
-
-    const styles = StyleSheet.create({
-        safeAreaContainer: {
-            flex: 1,
-        },
-        container: {
-            flex: 1,
-            backgroundColor: COLORS.background,
-            alignItems: 'center',
-            justifyContent: 'center',
-        },
-        errorText: {
-            color: '#F00',
-            textAlign: 'center',
-            marginVertical: 35,
-        },
-        text: {
-            textAlign: 'center',
-            color: COLORS.secondText,
-        },
-        link: {
-            color: COLORS.elements,
-            fontWeight: 'bold',
-        },
-        title: {
-            color: COLORS.elements,
-            fontSize: 65,
-            marginBottom: 120,
-            textAlign: 'center',
-            fontFamily: 'mnst-bold'
-        },
-        image: {
-            // flex: 1,
-            width: 80,
-            height: 80,
-            alignSelf: 'center',
-            // resizeMode: 'cover',
-        }
-    });
-
-    return (
-        <SafeAreaView style={styles.safeAreaContainer}>
-            <View style={styles.container}>
-                <View>
-                    <Image source={logo} style={styles.image} />
-                    <Text style={styles.title}>SIGN UP</Text>
-                    <GNTextInput
-                        placeholder='Username'
-                        iconName="person-outline"
-                        iconNameFocused="person"
-                        onChangeText={setUsername}
-                        animation="true" />
-                    <GNTextInput
-                        placeholder='Email'
-                        iconName="mail-outline"
-                        iconNameFocused="mail"
-                        onChangeText={setEmail}
-                        animation="true" />
-                    <GNTextInputPassword
-                        placeholder='Password'
-                        iconName="lock-closed-outline"
-                        iconNameFocused="lock-closed"
-                        onChangeText={setPassword}
-                        animation="true" />
-                    <GNTextInputPassword
-                        placeholder='Confirm Password'
-                        iconName="lock-closed-outline"
-                        iconNameFocused="lock-closed"
-                        onChangeText={setPassword2}
-                        animation="true"
-                        marginBottom={60} />
-                    <GNButton
-                        title={"SIGN UP"}
-                        onPress={() => handleRegister(
-                            username,
-                            email,
-                            password,
-                            password2,
-                            navigation,
-                            setError
-                        )}
-                    />
-                    <Text style={styles.text}>
-                        Already have an account?
-                        <Text style={styles.link} onPress={() => { navigation.navigate(ROUTES.LOGIN) }}> Sign in!</Text>
-                    </Text>
-                    {error}
-                    <StatusBar style="dark" />
-                </View>
-            </View >
-        </SafeAreaView>
-    )
+  return (
+    <SafeAreaView style={styles.safeAreaContainer}>
+        <KeyboardAvoidingView
+          behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+          style={styles.keyboard}
+        >
+          <ScrollView
+            contentContainerStyle={styles.scrollViewContainer}
+            keyboardShouldPersistTaps="handled"
+          >
+            <Image source={logo} style={styles.image} />
+            <Text style={styles.title}>SIGN UP</Text>
+            <GNTextInput
+              placeholder="Username"
+              iconName="person-outline"
+              iconNameFocused="person"
+              onChangeText={setUsername}
+              animation="true"
+            />
+            <GNTextInput
+              placeholder="Email"
+              iconName="mail-outline"
+              iconNameFocused="mail"
+              onChangeText={setEmail}
+              animation="true"
+            />
+            <GNTextInputPassword
+              placeholder="Password"
+              iconName="lock-closed-outline"
+              iconNameFocused="lock-closed"
+              onChangeText={setPassword}
+              animation="true"
+            />
+            <GNTextInputPassword
+              placeholder="Confirm Password"
+              iconName="lock-closed-outline"
+              iconNameFocused="lock-closed"
+              onChangeText={setPassword2}
+              animation="true"
+              marginBottom={45}
+            />
+            <GNButton
+              title="SIGN UP"
+              onPress={() =>
+                handleRegister(username, email, password, password2, navigation, setError)
+              }
+            />
+            <Text></Text>
+            <Text style={styles.text}>
+              Already have an account?
+              <Text
+                style={styles.link}
+                onPress={() => {
+                  navigation.navigate(ROUTES.LOGIN);
+                }}
+              >
+                {' '}
+                Sign in!
+              </Text>
+            </Text>
+            {error}
+            <StatusBar style="dark" />
+          </ScrollView>
+        </KeyboardAvoidingView>
+    </SafeAreaView>
+  );
 }
